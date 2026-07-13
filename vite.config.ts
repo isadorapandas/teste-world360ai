6f03bef46b4864d581f2a5533da11e0cd9dfd95e
isadorapandas <202869015+isadorapandas@users.noreply.github.com>
2026-07-12 22:05:22 -0300

    Att vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/world360-ai/',
+  base: '/teste-world360ai/',
   plugins: [react()],
 })
