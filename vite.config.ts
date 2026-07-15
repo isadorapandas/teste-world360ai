b30fc2a40594208a8c3710d91005a654e7f8a30b
isadorapandas <202869015+isadorapandas@users.noreply.github.com>
2026-07-15 17:44:55 -0300

    Integrating HTML pages into the Vite ecosystem

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/teste-world360ai/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        highlights: resolve(__dirname, 'highlights.html'),
+        research: resolve(__dirname, 'research.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        events: resolve(__dirname, 'events.html'),
+        media: resolve(__dirname, 'media.html'),
+        references: resolve(__dirname, 'references.html'),
+        internal: resolve(__dirname, 'internal.html')
+      },
+    },
+  },
 })
